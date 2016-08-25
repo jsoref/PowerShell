4e7ca8232bc40c9f8a93a2b7e1605726024c7621
Josh Soref <jsoref@users.noreply.github.com>
2016-08-25 06:05:00 +0000

    spelling: unaffected

--- src/System.Management.Automation/engine/MshObjectTypeDescriptor.cs
+++ src/System.Management.Automation/engine/MshObjectTypeDescriptor.cs
@@ -727,7 +727,7 @@ namespace System.Management.Automation
             // an PSObjectTypeDescriptor(null). PSObjectTypeDescriptor's GetProperties 
             // checks for null instance and returns an empty property collection. 
             // All other overrides also check for null and return some default result.
-            // Case 1), which is using a PropertyGrid seems to be unafected by these results returned
+            // Case 1), which is using a PropertyGrid seems to be unaffected by these results returned
             // by PSObjectTypeDescriptor overrides when the Instance is null, so we must conclude
             // that the TypeDescriptor returned by that call where instance is null is not used
             // for anything meaningful. That null instance PSObjectTypeDescriptor is only one
