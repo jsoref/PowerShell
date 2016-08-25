ea8c1a8c0e57613ade024abd98ec74ad52edc3dc
Josh Soref <jsoref@users.noreply.github.com>
2016-08-25 04:19:25 +0000

    spelling: they-are

--- src/System.Management.Automation/engine/interpreter/LightCompiler.cs
+++ src/System.Management.Automation/engine/interpreter/LightCompiler.cs
@@ -1249,7 +1249,7 @@ namespace System.Management.Automation.Interpreter
                     return true;
                 case ExpressionType.Switch:
                     PushLabelBlock(LabelScopeKind.Switch);
-                    // Define labels inside of the switch cases so theyare in
+                    // Define labels inside of the switch cases so they are in
                     // scope for the whole switch. This allows "goto case" and
                     // "goto default" to be considered as local jumps.
                     var @switch = (SwitchExpression)node;
